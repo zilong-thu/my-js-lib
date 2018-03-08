c7fa07e92c502431551e3943ac2bfb361e90dbc8
王子龙 <wangzilong03@maoyan.com>
2018-03-08 10:57:40 +0800

    add

--- finance/salary-tax.js
+++ finance/salary-tax.js
@@ -8,22 +8,25 @@ function taxCalculator(options = {
   income: 10000,
   profit: 0,
   desc: '',
+  showConsoles: false,
 }) {
   const base = options.base || 3500;
-  console.log(`---------${options.desc || ''}---------\n个税起征点: `, base);
+  const desc = options.desc || '';
+  const showConsoles = options.showConsoles || false;
 
+  // 税前收入
   const income = options.income || 10000;
-  console.log('税前收入: ', income);
 
+  // 五险一金缴纳额
   const profit = options.profit || Math.round(income * 0.202);
-  console.log('五险一金缴纳额: ', profit);
 
   const income_after_profit = Math.round(income - profit);
+
+  // 应纳税所得额
   let income_for_tax = income_after_profit - base;
   if (income_for_tax < 0) {
     income_for_tax = 0;
   }
-  console.log('应纳税所得额: ', income_for_tax);
 
   // 应纳税所得额(不含税)表
   const list = [1455, 4155, 7755, 27255, 41255, 57505, 1000000000000];
@@ -40,19 +43,21 @@ function taxCalculator(options = {
   if (income_for_tax) {
     tax = Math.round(income_for_tax * rate[index] - minus[index]);
   }
-  console.log('纳税额: ', tax);
 
+  // 税后收入
   const real_income = income_after_profit - tax;
-  console.log('税后收入: ', real_income, '\n------------------\n');
 
-  return {income, real_income};
+  if (showConsoles) {
+    console.log(`---------${desc || ''}---------\n个税起征点: `, base);
+    console.log('税前收入: ', income);
+    console.log('纳税额: ', tax);
+    console.log('税后收入: ', real_income, '\n------------------\n');
+  }
+
+  return {income, real_income, desc, tax};
 }
 
-const arr = [];
-for (var i = 10000; i < 100000; i += 1000) {
-  arr.push(i);
-}
-
-taxCalculator({base: 3500, income: 9000, desc: '现行税率'});
-taxCalculator({base: 8000, income: 9000, desc: '全国工商联提案'});
-taxCalculator({base: 10000, income: 9000, desc: '董明珠的提案'});
+// taxCalculator({base: 3500, income: 25000, desc: '现行税率'});
+// taxCalculator({base: 8000, income: 25000, desc: '全国工商联提案'});
+// taxCalculator({base: 10000, income: 25000, desc: '董明珠的提案'});
+// taxCalculator({base: 12000, income: 25000, desc: '我的预测'});
