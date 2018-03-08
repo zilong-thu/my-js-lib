55e28d94b35a9ef3c7389f17e3318acff78bb9e3
王子龙 <wangzilong03@maoyan.com>
2018-03-08 11:06:33 +0800

    fix

--- finance/salary-tax.js
+++ finance/salary-tax.js
@@ -29,11 +29,11 @@ function taxCalculator(options = {
   }
 
   // 应纳税所得额(不含税)表
-  const list = [1455, 4155, 7755, 27255, 41255, 57505, 1000000000000];
+  const list = [0, 1455, 4155, 7755, 27255, 41255, 57505, 1000000000000];
   // 税率表
-  const rate = [0.03, 0.1, 0.20, 0.25, 0.3, 0.35, 0.45];
+  const rate = [0, 0.03, 0.1, 0.20, 0.25, 0.3, 0.35, 0.45];
   // 速算扣除数表
-  const minus = [0, 105, 555, 1005, 2775, 5505, 13505];
+  const minus = [0, 0, 105, 555, 1005, 2775, 5505, 13505];
 
   let index = list.findIndex((item, i) => {
     return (item > income_for_tax) && (list[i - 1] < income_for_tax);
@@ -50,6 +50,7 @@ function taxCalculator(options = {
   if (showConsoles) {
     console.log(`---------${desc || ''}---------\n个税起征点: `, base);
     console.log('税前收入: ', income);
+    console.log('应纳税所得额: ', income_for_tax);
     console.log('纳税额: ', tax);
     console.log('税后收入: ', real_income, '\n------------------\n');
   }
@@ -60,4 +61,4 @@ function taxCalculator(options = {
 // taxCalculator({base: 3500, income: 25000, desc: '现行税率'});
 // taxCalculator({base: 8000, income: 25000, desc: '全国工商联提案'});
 // taxCalculator({base: 10000, income: 25000, desc: '董明珠的提案'});
-// taxCalculator({base: 12000, income: 25000, desc: '我的预测'});
+taxCalculator({base: 14000, income: 18000, desc: '我的预测', showConsoles: true});
